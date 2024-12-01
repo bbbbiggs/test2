cb0fbec060883e78262ac9d893cc106530df6e13
biggsky <sunghyun0624@naver.com>
2024-12-01 23:04:49 +0900

    cors edit

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,14 +6,23 @@ const app = express();
 
 app.use(express.json());
 
+// app.use(
+//   cors({
+//     // origin: ["http://localhost:3000"],
+//     // origin: ["http://mysssbuckettt.s3-website.ap-northeast-2.amazonaws.com"],
+
+//     // origin: "https://d1mgx07wg6eqil.cloudfront.net",
+//     origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    // origin: ["http://localhost:3000"],
-    // origin: ["http://mysssbuckettt.s3-website.ap-northeast-2.amazonaws.com"],
-
-    // origin: "https://d1mgx07wg6eqil.cloudfront.net",
+    // origin: [process.env.CORS, process.env.CORS_DEV],
     origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   })
 );
 
