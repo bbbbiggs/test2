b09c4f0afb27a05d366a0653e3ba2bc5ad25c4b3
biggsky <sunghyun0624@naver.com>
2024-12-01 20:50:06 +0900

    1201

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,29 +6,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://d1mgx07wg6eqil.cloudfront.net"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-app.get("/getList", (req, res) => {
-  res.json({ message: "CORS is working!" });
-});
-
 app.use(
   cors({
     // origin: ["http://localhost:3000"],
     // origin: ["http://mysssbuckettt.s3-website.ap-northeast-2.amazonaws.com"],
 
-    origin: "https://d1mgx07wg6eqil.cloudfront.net",
+    // origin: "https://d1mgx07wg6eqil.cloudfront.net",
+    origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
     credentials: true,
   })
 );
