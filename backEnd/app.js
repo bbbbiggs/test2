3cc0f319bb387c220f5aa850fbab1fc3cfe4375c
biggsky <sunghyun0624@naver.com>
2024-12-01 20:52:51 +0900

    * add

--- backEnd/app.js
+++ backEnd/app.js
@@ -12,7 +12,8 @@ app.use(
     // origin: ["http://mysssbuckettt.s3-website.ap-northeast-2.amazonaws.com"],
 
     // origin: "https://d1mgx07wg6eqil.cloudfront.net",
-    origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
+    // origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
+    origin: "*",
     credentials: true,
   })
 );
