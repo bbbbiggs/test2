056e02820eeff1b023d4c37bb7e6ef16665c991a
biggsky <sunghyun0624@naver.com>
2024-12-02 23:23:08 +0900

    1202

--- backEnd/app.js
+++ backEnd/app.js
@@ -3,23 +3,23 @@ const dot = require("dotenv").config();
 const { sequelize } = require("./models");
 const cors = require("cors");
 const app = express();
+const mainRouter = require("./routers/mainRouter");
 
 app.use(express.json());
+app.use("/", mainRouter);
 
-app.use(
-  cors({
-    // origin: ["http://localhost:3000"],
-    // origin: ["http://mysssbuckettt.s3-website.ap-northeast-2.amazonaws.com"],
+// app.use(
+//   cors({
+//     // origin: ["http://localhost:3000"],
+//     // origin: ["http://mysssbuckettt.s3-website.ap-northeast-2.amazonaws.com"],
 
-    // origin: "https://d1mgx07wg6eqil.cloudfront.net",
-    // origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
-    origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
-    credentials: true,
-  })
-);
-app.options("*", cors());
-
-const mainRouter = require("./routers/mainRouter");
+//     // origin: "https://d1mgx07wg6eqil.cloudfront.net",
+//     // origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
+//     origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 
 sequelize
   .sync({ force: false })
@@ -29,8 +29,6 @@ sequelize
   .catch((err) => {
     console.log(err);
   });
-
-app.use("/", mainRouter);
 
 const server = app.listen(8000, () => {
   console.log("server on");
