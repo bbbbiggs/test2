e694e285156d93c759568f9ec1f4a5fb6e19259c
biggsky <sunghyun0624@naver.com>
2024-12-02 23:16:31 +0900

    1202

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,17 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 app.use(
   cors({
     // origin: ["http://localhost:3000"],
@@ -28,6 +17,7 @@ app.use(
     credentials: true,
   })
 );
+app.options("*", cors());
 
 const mainRouter = require("./routers/mainRouter");
 
@@ -41,17 +31,6 @@ sequelize
   });
 
 app.use("/", mainRouter);
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
 
 const server = app.listen(8000, () => {
   console.log("server on");
