754b0bbe23b5b03bca3fe6c854128d6067b18b72
biggsky <sunghyun0624@naver.com>
2024-12-02 22:54:26 +0900

    :80

--- backEnd/app.js
+++ backEnd/app.js
@@ -12,8 +12,8 @@ app.use(
     // origin: ["http://mysssbuckettt.s3-website.ap-northeast-2.amazonaws.com"],
 
     // origin: "https://d1mgx07wg6eqil.cloudfront.net",
-    origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app:3000",
-    // origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app:80",
+    // origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
+    origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app:80",
     credentials: true,
   })
 );
