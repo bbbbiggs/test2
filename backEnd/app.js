3321191e8ea18776ded5f4016b22348076b6fab6
biggsky <sunghyun0624@naver.com>
2024-12-03 23:26:05 +0900

    4th commit

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,29 +6,17 @@ const app = express();
 
 app.use(express.json());
 
-const cookieParser = require("cookie-parser");
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 app.use(
   cors({
     // origin: ["http://localhost:3000"],
     // origin: ["http://mysssbuckettt.s3-website.ap-northeast-2.amazonaws.com"],
 
-    // origin: "https://d1mgx07wg6eqil.cloudfront.net",
-    // origin: "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
-    origin: ["https://web-front-m3viba9m327f6c46.sel4.cloudtype.app"],
+    origin: [
+      "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
+      "https://port-0-test2-m3viba9m327f6c46.sel4.cloudtype.app",
+    ],
     credentials: true,
-  })
-);
-
-app.use(
-  session({
-    // secret: process.env.SESSION_KEY,
-    resave: false,
-    saveUninitialized: false,
-    proxy: true,
-    cookie: { sameSite: "none", secure: true, maxAge: 600000, httpOnly: true }, // 이 부분에서 secure 옵션을 true로 설정합니다.
+    optionsSuccessStatus: 200,
   })
 );
 
