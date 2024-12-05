15aa02b97e523a506f9bfa2f0566ca303b7ba9d7
biggsky <sunghyun0624@naver.com>
2024-12-05 23:52:48 +0900

    1205

--- backEnd/app.js
+++ backEnd/app.js
@@ -7,24 +7,20 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
 app.use(
   cors({
     // origin: ["http://localhost:3000"],
-    // origin: ["http://mysssbuckettt.s3-website.ap-northeast-2.amazonaws.com"],
-
-    origin: [
-      // "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app",
-      // "https://d1mgx07wg6eqil.cloudfront.net/",
-      "https://web-front-m3viba9m327f6c46.sel4.cloudtype.app/",
-    ],
+    origin: ["https://web-front-m3viba9m327f6c46.sel4.cloudtype.app"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
 );
+
+app.options("*", cors());
 
 const mainRouter = require("./routers/mainRouter");
 
