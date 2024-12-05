8951daea01902e0a0682559bfabed9aed6e8929a
biggsky <sunghyun0624@naver.com>
2024-12-05 23:57:09 +0900

    1205

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,21 +6,15 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
-
 app.use(
   cors({
     // origin: ["http://localhost:3000"],
     origin: ["https://web-front-m3viba9m327f6c46.sel4.cloudtype.app"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 200,
   })
 );
-
-app.options("*", cors());
 
 const mainRouter = require("./routers/mainRouter");
 
